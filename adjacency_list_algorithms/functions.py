2d7a3f897cc2ef4b9d0fe868b0d207819d9e4b73
whiterabbitholes <129126175+whiterabbitholes@users.noreply.github.com>
2023-03-29 10:18:01 -0300

    check if is suggraph adjacency function

--- adjacency_list_algorithms/functions.py
+++ adjacency_list_algorithms/functions.py
@@ -13,3 +13,18 @@ def get_vertices_do_grau(grafo, grau):
             pass
             
     return resposta
+
+    def check_is_subgraph(grafo1, grafo2):
+        for vertice in grafo2:
+            if vertice not in grafo1:
+                return False;
+            else:
+                adjacency_graph1 = grafo1[vertice]
+                adjacency_graph2 = graph2[vertice]
+                for neighboor in adjacency_graph2:
+                    if neighboor not in adjacency_graph1:
+                        return False
+                    else:
+                        pass
+
+        return True
