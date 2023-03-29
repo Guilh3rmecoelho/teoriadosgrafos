5ba5c879eb9b3e2b525deccb86428a8ffc4a7cb7
whiterabbitholes <129126175+whiterabbitholes@users.noreply.github.com>
2023-03-29 10:13:22 -0300

    TG3 slide4 exercise

---
+++ adjacency_list_algorithms/exercicios/slide3.1.py
@@ -0,0 +1,11 @@
+
+
+from .. import grafos[0] as grafo
+from ../functions import get_grau, get_vertices_do_grau
+
+
+# Exercício 3.5.1
+print(f'O grau é {get_grau(grafo, 1)}')
+
+# Exercício 3.5.2
+prtin(get_vertices_do_grau(grafo, 1))
